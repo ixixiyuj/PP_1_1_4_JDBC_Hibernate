1acffed468319e36649094d5b17c2d959cbc19f3
ixixiyuj <kozlowa.d@gmail.com>
2022-10-05 19:45:36 +0300

    1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,4 +1,6 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
@@ -14,7 +16,7 @@ public class Main {
 //    Удаление таблицы
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Util.getInstance().getConnection();
+//        Util.getInstance().getConnection();
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         for (int i = 0; i < 4; i ++) {
@@ -27,6 +29,7 @@ public class Main {
         userService.getAllUsers().forEach(System.out::println);
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,96 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
+
     }
+
 
 
     @Override
     public void createUsersTable() {
+    try (Session session = Util.getSessionFactory().openSession()) {
+        String sql = new StringBuilder().append("CREATE TABLE IF NOT EXISTS users ")
+                .append("(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, ")
+                .append("name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, ")
+                .append("age TINYINT NOT NULL)").toString();;
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+        session.createSQLQuery(sql)
+                .addEntity(User.class)
+                .executeUpdate();
+        transaction.commit();
+
+    }
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS users")
+                .addEntity(User.class)
+                .executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        SessionFactory sf = Util.getSessionFactory();
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        User user = new User(name, lastName, age);
+        session.save(user);
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public void removeUserById(long id) {
+        SessionFactory sf = Util.getSessionFactory();
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        String sql = "delete from User where id = ?";
+        session.createQuery("delete from User where id = :par").setParameter("par", id).executeUpdate();
 
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        SessionFactory sf = Util.getSessionFactory();
+        Session session = sf.openSession();
+        List<User> users = session.createQuery("FROM User", User.class).list();
+        session.close();
+        return users;
     }
+
 
     @Override
     public void cleanUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("TRUNCATE users")
+                .addEntity(User.class)
+                .executeUpdate();
+        transaction.commit();
+        session.close();
 
     }
 }
